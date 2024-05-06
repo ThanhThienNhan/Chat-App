65ad2f3e5d6a98aea443f0a336920b19db6f7a2f
21127535 <thanhthiennhan2003@gmail.com>
2024-05-06 16:05:54 +0700

    Setup routes and controllers

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+const signup=(req,res)=>{
+    console.log("signupUser");
+}
+
+const login=(req,res)=>{
+    console.log("loginUser");
+}
+
+const logout=(req,res)=>{
+    console.log("logoutUser");
+}
+
+module.exports={signup,login,logout}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-const express= require('express')
+const express= require('express');
+const dotenv=require("dotenv");
+const authRoutes=require("./routes/auth.routes")
+const app =express();
+
+dotenv.config();
+const PORT = process.env.PORT||5000;
+
+app.get("/",(req,res)=>{
+    res.send("Hello World!")
+})
+
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT,()=>console.log(`Server Running on port ${PORT}`));
