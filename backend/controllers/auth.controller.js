829fa827a310cd1b99067da7b4a0fcbc2223315e
21127535 <thanhthiennhan2003@gmail.com>
2024-05-23 15:05:29 +0700

    Get conversations to sidebar

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -80,7 +80,7 @@ const login=async(req,res)=>{
 
 const logout=(req,res)=>{
     try {
-        res.cookie("jwt","",{maxAge:0});
+        res.clearCookie("jwt");
         res.status(200).json({message:"Logged out successfully"})
     } catch (error) {
         console.log("Error in logout controller",error.message);
