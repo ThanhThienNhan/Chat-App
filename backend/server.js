2a2c6c7bf8796f850c11b36a67b6858155e80310
21127535 <thanhthiennhan2003@gmail.com>
2024-05-26 09:20:41 +0700

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,36 @@
-const express= require('express');
-const dotenv=require("dotenv");
-const cookieParser=require("cookie-parser")
+const path = require('path');
+const express = require('express');
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser")
 
-const authRoutes=require("./routes/auth.routes");
-const messageRoutes=require("./routes/message.routes")
-const userRoutes=require("./routes/user.routes");
+const authRoutes = require("./routes/auth.routes");
+const messageRoutes = require("./routes/message.routes")
+const userRoutes = require("./routes/user.routes");
 
 const connectToMongoDB = require('./db/connectToMongoDB');
 
-const {app,server}=require('./socket/socket')
+const { app, server } = require('./socket/socket')
 
-const PORT = process.env.PORT||5000;
+__dirname = path.resolve();
+
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
 app.use(express.json());//to parse requests with JSON payloads (from req.body) 
 app.use(cookieParser());
 
-// app.get("/",(req,res)=>{
-//     res.send("Hello World!")
-// })
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
-app.use("/api/users",userRoutes);
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend', 'dist', 'index.html'));
+})
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server Running on port ${PORT}`);
 });
--- frontend/src/components/messagecontainer/MessageContainer.jsx
+++ frontend/src/components/messagecontainer/MessageContainer.jsx
@@ -16,7 +16,6 @@ function MessageContainer() {
         <div className='md:min-w-[450px] flex flex-col'>
             {selectedConversation ? (
                 <>
-                    {/* Header */}
                     <div className='bg-slate-500 px-4 py-2 mb-2'>
                         <span className='label-text'>To:</span>
                         <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
