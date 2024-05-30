37bb587644eac59a32e60a014a4eef4ddf42b6c6
21127535 <thanhthiennhan2003@gmail.com>
2024-05-30 17:29:28 +0700

    Insert emoji by input cursor

--- frontend/src/components/messagecontainer/MessageInput.jsx
+++ frontend/src/components/messagecontainer/MessageInput.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { BsSend } from 'react-icons/bs';
 import { GrEmoji } from 'react-icons/gr';
 import EmojiPicker from 'emoji-picker-react';
@@ -7,6 +7,7 @@ import useSendMessage from '../../hooks/useSendMessage';
 function MessageInput() {
     const [message, setMessage] = useState("");
     const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+    const inputRef = useRef(null);
 
     const { loading, sendMessage } = useSendMessage();
 
@@ -20,22 +21,36 @@ function MessageInput() {
     };
 
     const onEmojiClick = (emojiObject) => {
-        setMessage((prevMessage) => prevMessage + emojiObject.emoji);
-        //setShowEmojiPicker(false);
+        const cursor = inputRef.current.selectionStart;
+        setMessage(prevMessage => {
+            const newMessage = prevMessage.slice(0, cursor) + emojiObject.emoji + prevMessage.slice(cursor);
+
+            const newCursor = cursor + emojiObject.emoji.length;
+
+            setTimeout(() => inputRef.current.setSelectionRange(newCursor, newCursor), 10);
+
+            return newMessage;
+        });
     };
+
+    // const onEmojiClick = (emojiObject) => {
+    //     setMessage((prevMessage) => prevMessage + emojiObject.emoji);
+    //     //setShowEmojiPicker(false);
+    // };
 
     return (
         <form className='px-4 my-3' onSubmit={handleSubmit}>
             <div className='w-full relative'>
                 <input
                     type='text'
-                    className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
+                    className='border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white'
                     placeholder='Send a message'
                     value={message}
                     onChange={(e) => setMessage(e.target.value)}
+                    ref={inputRef}
                 />
                 <button type='button' className='absolute inset-y-0 end-10 flex items-center pe-3' onClick={() => setShowEmojiPicker(!showEmojiPicker)}>
-                    <GrEmoji/>
+                    <GrEmoji />
                 </button>
                 <button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
                     {loading ? <div className='loading loading-spinner'></div> : <BsSend />}
